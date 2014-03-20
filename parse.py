6ce97b8bf5ef0af0bbd7f3df9a9d5f52d0d4b237
Nick Hirakawa <nh0815@gmail.com>
2014-03-20 18:45:31 -0400

    Added corpus and query files. Implemented CorpusParser.

--- parse.py
+++ parse.py
@@ -1,1 +1,23 @@
 __author__ = 'Nick Hirakawa'
+
+import re
+
+
+class CorpusParser:
+
+	def __init__(self, filename):
+		self.filename = filename
+		self.regex = re.compile('^#\s*\d+')
+
+	def parse(self):
+		with open(self.filename) as f:
+			s = ''.join(f.readlines())
+		blobs = s.split('#')[1:]
+		for x in blobs:
+			split = x.split()
+			docid = split.pop(0)
+
+
+if __name__ == '__main__':
+	cp = CorpusParser('text/tccorpus.txt')
+	cp.parse()

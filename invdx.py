00cf88cb94974d9e2ae865b1382d44d6a6ffd826
Nick Hirakawa <nh0815@gmail.com>
2014-03-20 20:21:10 -0400

    Implemented InvertedIndex (untested). Implemented table for doc lengths (untested).

--- invdx.py
+++ invdx.py
@@ -3,3 +3,28 @@
 __author__ = 'Nick Hirakawa'
 
 
+class InvertedIndex:
+
+	def __init__(self):
+		self.index = dict()
+
+	def add(self, word, doc):
+		if word in self.index:
+			d = self.index[word]
+			d[doc] += 1
+		else:
+			d = dict()
+			d[doc] = 1
+			self.index[word] = d
+
+
+class DocumentLengthTable:
+
+	def __init__(self):
+		self.table = dict()
+
+	def add(self, doc, length):
+		self.table[doc] = length
+
+	def get_length(self, doc):
+		return self.table[doc]

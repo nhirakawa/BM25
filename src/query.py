d0b5a1250e087a27ac2e1df697d53efce7509d66
Nick Hirakawa <nh0815@gmail.com>
2014-03-21 17:41:35 -0400

    Skeleton for QueryProcessor class. Receives queries and corpus through constructor. Calls function to build data structures.

--- src/query.py
+++ src/query.py
@@ -1,1 +1,11 @@
 __author__ = 'Nick Hirakawa'
+
+
+from invdx import build_data_structures
+
+
+class QueryProcessor:
+
+	def __init__(self, queries, corpus):
+		self.queries = queries
+		self.index, self.dlt = build_data_structures(corpus)

9b7466574eee923a27b47b17b8cb41cc924442ea
Nick Hirakawa <nh0815@gmail.com>
2014-03-21 18:24:59 -0400

    Implemented query handling (untested).

--- src/query.py
+++ src/query.py
@@ -1,11 +1,27 @@
 __author__ = 'Nick Hirakawa'
 
-
 from invdx import build_data_structures
+from rank import score_BM25
 
 
 class QueryProcessor:
-
 	def __init__(self, queries, corpus):
 		self.queries = queries
 		self.index, self.dlt = build_data_structures(corpus)
+
+	def run(self):
+		for query in self.queries:
+			self.run_query(query)
+
+	def run_query(self, query):
+		for term in query:
+			query_result = dict()
+			if term in self.index:
+				doc_dict = self.index[query]
+				for docid, freq in doc_dict.iteritems():
+					score = score_BM25(n=len(doc_dict), f=freq, qf=1, r=0, N=len(self.dlt),
+									   dl=self.dlt.get_length(docid), avdl=self.dlt.get_average_length())
+					if docid in query_result:
+						query_result[docid] += score
+					else:
+						query_result[docid] = score

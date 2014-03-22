49094031bede2e281fb081f64a7ebc23476cb0e3
Nick Hirakawa <nh0815@gmail.com>
2014-03-22 00:51:47 -0400

    run_query now returns dictionary of document scores. run returns list of dictionary scores for all queries.

--- src/query.py
+++ src/query.py
@@ -2,6 +2,7 @@ __author__ = 'Nick Hirakawa'
 
 from invdx import build_data_structures
 from rank import score_BM25
+import operator
 
 
 class QueryProcessor:
@@ -10,14 +11,16 @@ class QueryProcessor:
 		self.index, self.dlt = build_data_structures(corpus)
 
 	def run(self):
+		results = []
 		for query in self.queries:
-			self.run_query(query)
+			results.append(self.run_query(query))
+		return results
 
 	def run_query(self, query):
 		query_result = dict()
 		for term in query:
 			if term in self.index:
-				doc_dict = self.index[query] # retrieve index entry
+				doc_dict = self.index[term] # retrieve index entry
 				for docid, freq in doc_dict.iteritems(): #for each document and its word frequency
 					score = score_BM25(n=len(doc_dict), f=freq, qf=1, r=0, N=len(self.dlt),
 									   dl=self.dlt.get_length(docid), avdl=self.dlt.get_average_length()) # calculate score
@@ -25,3 +28,4 @@ class QueryProcessor:
 						query_result[docid] += score
 					else:
 						query_result[docid] = score
+		return query_result

263f7798af38c174ccd1939920275faf72768e44
Nick Hirakawa <nh0815@gmail.com>
2014-03-22 00:52:42 -0400

    Central logic implemented. main.py calls parsers, sends queries and corpus to QueryProcessor, sorts results and displays.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 __author__ = 'Nick Hirakawa'
 
 
+from parse import *
+from query import QueryProcessor
+import operator
+
+
 def main():
-	pass
+	qp = QueryParser(filename='../text/queries.txt')
+	cp = CorpusParser(filename='../text/corpus.txt')
+	qp.parse()
+	queries = qp.get_queries()
+	cp.parse()
+	corpus = cp.get_corpus()
+	proc = QueryProcessor(queries, corpus)
+	results = proc.run()
+	for result in results:
+		sorted_x = sorted(result.iteritems(), key=operator.itemgetter(1))
+		sorted_x.reverse()
+		print len(sorted_x)
 
 
 if __name__ == '__main__':

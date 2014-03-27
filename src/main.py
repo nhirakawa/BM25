34e3da6c0816ac04c783a8a744dcc376350a8611
Nick Hirakawa <nh0815@gmail.com>
2014-03-27 01:50:36 -0400

    Modified output formatting.

--- src/main.py
+++ src/main.py
@@ -15,10 +15,16 @@ def main():
 	corpus = cp.get_corpus()
 	proc = QueryProcessor(queries, corpus)
 	results = proc.run()
+	qid = 0
 	for result in results:
 		sorted_x = sorted(result.iteritems(), key=operator.itemgetter(1))
 		sorted_x.reverse()
-		print len(sorted_x)
+		index = 0
+		for i in sorted_x[:100]:
+			tmp = (qid, i[0], index, i[1])
+			print '{:>1}\tQ0\t{:>4}\t{:>2}\t{:>12}\tNH-BM25'.format(*tmp)
+			index += 1
+		qid += 1
 
 
 if __name__ == '__main__':
